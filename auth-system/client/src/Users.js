c89498677d011c34e9a339db983639b1a79bbdba
dprats <diego.prats@gmail.com>
2018-05-30 11:44:32 -0700

    Html fix to /register component

---
+++ auth-system/client/src/Users.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+// class Users extends Component {
+//   // state = {users: []}
+//
+//   // componentDidMount() {
+//   //   fetch('/users')
+//   //     .then(res => res.json())
+//   //     .then(users => this.setState({ users }));
+//   // }
+//
+//   render() {
+//     return (
+//       <div >
+//         <div class="container row">
+//           <div class="jumbotron col-sm-4 pull-center">
+//             <form action="/signup" method="post">
+//               <div>
+//                 <label>Username:</label>
+//                 <input type="text" name="username"/>
+//               </div>
+//               <div>
+//                 <label>Email:</label>
+//                 <input type="text" name="email"/>
+//               </div>
+//               <div>
+//                 <label>Password:</label>
+//                 <input type="password" name="password"/>
+//               </div>
+//               <div>
+//                 <input class="btn btn-primary" type="submit" value="Sign Up"/>
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+class Users extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Users;
