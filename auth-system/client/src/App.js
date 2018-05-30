342c67a1829fd646a4c860d7ffb2a0b3a90a08b9
dprats <diego.prats@gmail.com>
2018-05-30 11:30:50 -0700

    wire up basic router and routes for React app

--- auth-system/client/src/App.js
+++ auth-system/client/src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-class App extends Component {
-  state = {users: []}
+const Home = () => <h1>Home</h1>
+const Users = () => <h1>Users</h1>
+const SignUp = () => <h1>SignUp</h1>
+const Login = () => <h1>Login</h1>
 
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/users" component={Users}/>
+      <Route path="/signup" component={SignUp}/>
+      <Route path="/login" component={Login}/>
+    </div>
+  </Router>
+);
+
+// class App extends Component {
+//   state = {users: []}
+//
+//   componentDidMount() {
+//     fetch('/users')
+//       .then(res => res.json())
+//       .then(users => this.setState({ users }));
+//   }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Users</h1>
+//         {this.state.users.map(user =>
+//           <div key={user.id}>{user.username}</div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
