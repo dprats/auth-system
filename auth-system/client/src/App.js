91b81122fb50d547ab341626465d8d80b49b181b
dprats <diego.prats@gmail.com>
2018-05-31 00:32:20 -0700

    add a login and logout button in App.js

--- auth-system/client/src/App.js
+++ auth-system/client/src/App.js
@@ -3,17 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import AuthService from './components/AuthService';
 import withAuth from './components/withAuth.js';
+import { Link } from 'react-router-dom';
+
 const Auth = new AuthService();
 
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+  }
 
   handleLogout(){
     Auth.logout();
     this.props.history.replace('/login');
   }
 
-
   render() {
+
+    const isLoggedIn = this.state.isLoggedIn;
+
     return (
       <div className="App">
           <div className="App-header">
@@ -21,7 +31,11 @@ class App extends Component {
             <h2>Welcome {this.props.user.username}</h2>
           </div>
         <p className="App">
-          <button type="button" className="form-submit" onClick={this.handleLogout.bind(this)}>Logout</button>
+          { isLoggedIn ? (
+              <button type="button" className="form-submit" onClick={this.handleLogout.bind(this)}>Logout</button>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
         </p>
       </div>
     );
