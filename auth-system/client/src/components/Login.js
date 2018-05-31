276eeb9ab789ae13e489f69f066ee1f9c7b2db7e
dprats <diego.prats@gmail.com>
2018-05-30 23:55:20 -0700

    create a component for logging in

---
+++ auth-system/client/src/components/Login.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+// import { Redirect } from 'react-router-dom';
+import './Login.css';
+import AuthService from './AuthService';
+
+
+class Login extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.Auth = new AuthService();
+  }
+
+  handleFormSubmit(e){
+    e.preventDefault();
+
+    this.Auth.login(this.state.username,this.state.password)
+      .then(res =>{
+
+        this.props.history.replace('/dashboard');
+
+      })
+      .catch(err =>{
+        alert(err);
+      })
+  }
+
+  //Add redirection if we are already loggedIn
+  componentWillMount(){
+    if(this.Auth.loggedIn())
+      this.props.history.replace('/');
+  }
+
+  render() {
+    return (
+      <div className="center-login">
+        <div className="card">
+          <h1>Login</h1>
+          <form onSubmit={this.handleFormSubmit}>
+            <input
+              className="form-item"
+              placeholder="Username goes here..."
+              name="username"
+              type="text"
+              onChange={this.handleChange}
+            />
+            <input
+              className="form-item"
+              placeholder="Password goes here..."
+              name="password"
+              type="password"
+              onChange={this.handleChange}
+            />
+            <input
+              className="form-submit-login"
+              value="SUBMIT"
+              type="submit"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+  handleChange(e){
+    this.setState(
+      {
+        [e.target.name]: e.target.value
+      }
+    )
+  }
+}
+
+export default Login;
