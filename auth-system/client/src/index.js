db6a63c2793a72625da8b2bf6e4ca6ba72ded81b
dprats <diego.prats@gmail.com>
2018-05-30 23:04:06 -0700

    add login and signup routes to index.js on react client

--- auth-system/client/src/index.js
+++ auth-system/client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './App';
+import './index.css';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import Login from './components/Login.js';
+import Signup from './components/SignUp.js';
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/signup" component={Signup}/>
+
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
 registerServiceWorker();
